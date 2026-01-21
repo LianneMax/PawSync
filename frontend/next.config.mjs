f69ac99a73189d0352a9016f7688def69a3e6e18
LianneMax <165748673+LianneMax@users.noreply.github.com>
2026-01-21 23:50:14 +0800

    Fixed Combined npm run dev

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  outputFileTracingRoot: process.cwd(),
+  rewrites: async () => {
+    return {
+      beforeFiles: [
+        {
+          source: '/api/:path*',
+          destination: 'http://localhost:5000/api/:path*',
+        },
+      ],
+    };
+  },
+};
 
 export default nextConfig;
